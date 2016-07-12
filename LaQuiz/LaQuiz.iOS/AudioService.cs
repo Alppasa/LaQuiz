31f2b2e90ecddb8212a0e9b48dc6b73ce4084099
Alppasa <malpakduman@hotmail.de>
2016-07-12 21:20:58 +0200

    Add AudioService_IOS and Added Audio files

---
+++ LaQuiz/LaQuiz.iOS/AudioService.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using AudioToolbox;
+using Foundation;
+using LaQuiz.iOS;
+using LaQuiz.Interfaces;
+
+[assembly: Xamarin.Forms.Dependency(typeof(AudioService))]
+
+namespace LaQuiz.iOS
+{
+    class AudioService : IAudioService
+    {
+        public bool PlayFaildSound()
+        {
+            NSUrl url;
+            SystemSound systemSound;
+
+            url = NSUrl.FromFilename("FaildSound.mp3");
+            systemSound = new SystemSound(url);
+            systemSound.PlayAlertSound();
+            return true;
+        }
+
+        public bool PlayCorrectSound()
+        {
+            NSUrl url;
+            SystemSound systemSound;
+
+            url = NSUrl.FromFilename("applause.mp3");
+            systemSound = new SystemSound(url);
+            systemSound.PlayAlertSound();
+            return true;
+        }
+
+        public bool PlayHighScore()
+        {
+            NSUrl url;
+            SystemSound systemSound;
+
+            url = NSUrl.FromFilename("HighScore.mp3");
+            systemSound = new SystemSound(url);
+            systemSound.PlayAlertSound();
+            return true;
+        }
+
+        public bool PlayWinSound()
+        {
+            NSUrl url;
+            SystemSound systemSound;
+
+            url = NSUrl.FromFilename("FaildSound.mp3");
+            systemSound = new SystemSound(url);
+            systemSound.PlayAlertSound();
+            return true;
+        }
+
+        public bool PlayCountdown(bool on_off)
+        {
+            NSUrl url;
+            SystemSound systemSound;
+
+            url = NSUrl.FromFilename("countdown.mp3");
+            systemSound = new SystemSound(url);
+            systemSound.PlayAlertSound();
+            return true;
+        }
+    }
+}
